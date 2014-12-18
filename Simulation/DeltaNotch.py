5eab31edaecef832851f9d74c1f3e6ff41c23015
KaiYChen <wizard0429@gmail.com>
2014-12-18 17:53:28 -0500

    no polar cells + change energe

--- Simulation/DeltaNotch.py
+++ Simulation/DeltaNotch.py
@@ -6,46 +6,46 @@ sys.path.append(environ["PYTHON_MODULE_PATH"])
 sys.path.append(environ["SWIG_LIB_INSTALL_DIR"])
 
 def configureSimulation(sim):
-   import CompuCellSetup
-   from XMLUtils import ElementCC3D
-   cc3d=ElementCC3D("CompuCell3D")
-   
-   potts=cc3d.ElementCC3D("Potts")
-   potts.ElementCC3D("Dimensions",{"x":70,"y":150,"z":70})
-   potts.ElementCC3D("Steps",{},10000)
-   potts.ElementCC3D("Temperature",{},15)#original 30   
-   potts.ElementCC3D("NeighborOrder",{},3)
+    import CompuCellSetup
+    from XMLUtils import ElementCC3D
+    cc3d=ElementCC3D("CompuCell3D")
 
-   cellType=cc3d.ElementCC3D("Plugin",{"Name":"CellType"})
-   cellType.ElementCC3D("CellType", {"TypeName":"Medium","TypeId":"0"})
-   cellType.ElementCC3D("CellType", {"TypeName":"TypeA" ,"TypeId":"1"}) # Epithelial cells
-   cellType.ElementCC3D("CellType", {"TypeName":"BM" ,"TypeId":"2"})    # Meschymal cells
+    potts=cc3d.ElementCC3D("Potts")
+    potts.ElementCC3D("Dimensions",{"x":70,"y":150,"z":70})
+    potts.ElementCC3D("Steps",{},10000)
+    potts.ElementCC3D("Temperature",{},15)#original 30   
+    potts.ElementCC3D("NeighborOrder",{},3)
 
-# assign Energe
-   contact=cc3d.ElementCC3D("Plugin",{"Name":"Contact"})
-   contact.ElementCC3D("Energy", {"Type1":"Medium", "Type2":"Medium"},0)
-   contact.ElementCC3D("Energy", {"Type1":"Medium", "Type2":"TypeA"},8)
-   contact.ElementCC3D("Energy", {"Type1":"Medium", "Type2":"BM"},10)
-   contact.ElementCC3D("Energy", {"Type1":"TypeA",  "Type2":"TypeA"},2)
-   contact.ElementCC3D("Energy", {"Type1":"TypeA",  "Type2":"BM"},1)
-   contact.ElementCC3D("Energy", {"Type1":"BM", "Type2":"BM"},0)
-   contact.ElementCC3D("NeighborOrder",{},5)
+    cellType=cc3d.ElementCC3D("Plugin",{"Name":"CellType"})
+    cellType.ElementCC3D("CellType", {"TypeName":"Medium","TypeId":"0"})
+    cellType.ElementCC3D("CellType", {"TypeName":"TypeA" ,"TypeId":"1"}) # Epithelial cells
+    cellType.ElementCC3D("CellType", {"TypeName":"BM" ,"TypeId":"2"})    # Meschymal cells
 
-   volume = cc3d.ElementCC3D("Plugin",{"Name":"VolumeLocalFlex"})
-   ntp = cc3d.ElementCC3D("Plugin",{"Name":"NeighborTracker"})
-   epb = cc3d.ElementCC3D("Plugin",{"Name":"ExternalPotential"})
-   ctm = cc3d.ElementCC3D("Plugin",{"Name":"CenterOfMass"})
-   pit = cc3d.ElementCC3D("Plugin",{"Name":"PixelTracker"})
-   BPT = cc3d.ElementCC3D("Plugin",{"Name":"BoundaryPixelTracker"})
- 
-   uipd = cc3d.ElementCC3D("Steppable",{"Type":"UniformInitializer"})
-   region = uipd.ElementCC3D("Region")
-   region.ElementCC3D("BoxMin",{"x":6,  "y":6,  "z":6})
-   region.ElementCC3D("BoxMax",{"x":65,  "y":130,  "z":65})
-   region.ElementCC3D("Types",{},"TypeA")
-   region.ElementCC3D("Width", {},10)
-      
-   CompuCellSetup.setSimulationXMLDescription(cc3d)
+    # assign Energe
+    contact=cc3d.ElementCC3D("Plugin",{"Name":"Contact"})
+    contact.ElementCC3D("Energy", {"Type1":"Medium", "Type2":"Medium"},0)
+    contact.ElementCC3D("Energy", {"Type1":"Medium", "Type2":"TypeA"},8)
+    contact.ElementCC3D("Energy", {"Type1":"Medium", "Type2":"BM"},10)
+    contact.ElementCC3D("Energy", {"Type1":"TypeA",  "Type2":"TypeA"},5 )
+    contact.ElementCC3D("Energy", {"Type1":"TypeA",  "Type2":"BM"},1)
+    contact.ElementCC3D("Energy", {"Type1":"BM", "Type2":"BM"},0)
+    contact.ElementCC3D("NeighborOrder",{},5)
+
+    volume = cc3d.ElementCC3D("Plugin",{"Name":"VolumeLocalFlex"})
+    ntp = cc3d.ElementCC3D("Plugin",{"Name":"NeighborTracker"})
+    epb = cc3d.ElementCC3D("Plugin",{"Name":"ExternalPotential"})
+    ctm = cc3d.ElementCC3D("Plugin",{"Name":"CenterOfMass"})
+    pit = cc3d.ElementCC3D("Plugin",{"Name":"PixelTracker"})
+    BPT = cc3d.ElementCC3D("Plugin",{"Name":"BoundaryPixelTracker"})
+
+    uipd = cc3d.ElementCC3D("Steppable",{"Type":"UniformInitializer"})
+    region = uipd.ElementCC3D("Region")
+    region.ElementCC3D("BoxMin",{"x":6,  "y":6,  "z":6})
+    region.ElementCC3D("BoxMax",{"x":65,  "y":130,  "z":65})
+    region.ElementCC3D("Types",{},"TypeA")
+    region.ElementCC3D("Width", {},10)
+
+    CompuCellSetup.setSimulationXMLDescription(cc3d)
 
 import CompuCellSetup
 sim,simthread = CompuCellSetup.getCoreSimulationObjects()
